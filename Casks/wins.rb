34391fc698562e317df76ab74f5c3b87c9103bd8
EggsBlue <1719411461@qq.com>
2024-05-17 17:23:55 +0800

    Update wins.rb

--- Casks/wins.rb
+++ Casks/wins.rb
@@ -3,7 +3,7 @@ cask "wins" do
   version "1.6.6"
   sha256 "594fb383c26f3b36dfa132312ddbafb0e59f7566616b5f02294e769cecf55605"
 
-  url "https://f005.backblazeb2.com/file/winsWebsite/Wins-latest-1.6.6.dmg", verified: "backblazeb2.com/file/winsWebsite"
+  url "https://f005.backblazeb2.com/file/winsWebsite/Wins-latest-#{version}.dmg", verified: "backblazeb2.com/file/winsWebsite"
   name "Wins" 
   desc "a brand new window manager for macOS"
   homepage "https://wins.cool/"
